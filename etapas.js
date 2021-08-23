dce889bad5a932bbb9d191238bef78dbc57f932e
kleberkunha <kleberkunha@hotmail.com>
2021-08-23 12:29:40 +0200

    Upp eletronic vote Brazil

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+        {
+                titulo : 'VEREADOR',
+                numeros : 5,
+                candidatos: [
+                        {
+                                numero: '38111',
+                                nome : 'Fulado de Tal',
+                                partido: 'ABC',
+                                fotos: [
+                                        {url: 'vereador.png', legenda: 'Vereador'}
+                                ]
+                        },
+                        {
+                                numero: '72222',
+                                nome: 'Beltrano da Silva',
+                                partido: 'DFGH',
+                                fotos: [
+                                        {url :'vereador2.png', legenda:'Vereador'}
+                                ]
+                        }
+                ]
+        },
+        {
+                titulo: 'PREFEITO',
+                numeros : 2,
+                candidatos: [
+                        {
+                                numero: '99',
+                                nome:'Ciclano',
+                                partido: 'ABC',
+                                vice : 'CIC',
+                                fotos: [
+                                        {url: 'pref1.png', legenda: 'Prefeito'},
+                                        {url: 'vice1.png', legenda:'Vice-Prefeito', small: true}
+                                ]
+                        },
+                        {
+                                numero: '84',
+                                nome:'Zulano',
+                                partido: 'ABC',
+                                vice : 'CIC',
+                                fotos: [
+                                        {url: 'pref2.png', legenda: 'Prefeito'},
+                                        {url: 'vice2.png', legenda:'Vice-Prefeito', small: true}
+                                ]
+                        }
+
+                ]
+        }
+
+];
